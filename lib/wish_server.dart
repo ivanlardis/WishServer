88295eff99ee831308d195066b0d082bb4ef14b5
ivanlardis <ivan@lardis.ru>
2019-02-17 11:42:48 +0300

    - init

---
+++ lib/wish_server.dart
@@ -0,0 +1,11 @@
+/// wish_server
+///
+/// A Aqueduct web server.
+library wish_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
